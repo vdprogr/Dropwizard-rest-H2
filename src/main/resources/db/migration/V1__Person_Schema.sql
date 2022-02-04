ad1a0c3efaffccd391a3493158aa6c0a8bc2506a
Volodymyr Maliarchuk <vdprogr@gmail.com>
2022-02-04 13:16:41 +0200

    modified migrations

--- src/main/resources/db/migration/V1__Person_Schema.sql
+++ src/main/resources/db/migration/V1__Person_Schema.sql
@@ -3,7 +3,3 @@ create table person (
                        firstname varchar(100) not null,
                        lastname varchar(100) not null
 );
-
-insert into person (firstname, lastname) values ('Dan', 'Johnson');
-insert into person (firstname, lastname) values ('Tom', 'Nilson');
-insert into person (firstname, lastname) values ('Jim', 'Kerry');
